a8c2d22cd5483d70d7894ed0205335c22276265a
Fedor Parenkov <85241814+Fed-Parenkov@users.noreply.github.com>
2021-09-09 09:20:59 +0400

    Update Main.java

--- src/main/java/fedor/parenkov/Main.java
+++ src/main/java/fedor/parenkov/Main.java
@@ -4,9 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
 
-//      Human defaulHuman = new Human();
-//      defaulHuman.meetTheHuman();
-
         Human russianGirl = new Human("Mary", false, 28, 1.65,
         "Russia", true, "HR manager", false);
 
@@ -15,6 +12,7 @@ public class Main {
         russianGirl.abilityToBearChildren();
         russianGirl.haveBadHabits();
 
+        
        Human russianBoy = new Human("Alex", true, 30, 1.83,
        "Russia", false, "QA engineer", true);
 
